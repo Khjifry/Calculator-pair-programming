8aaa91532604d22a8c720097d08fa3ba5eb2fed6
Khjifry <khjifry@outlook.com>
2024-01-18 11:22:06 +0700

    Make sure the 'C' and 'CE' buttons work

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const keys = document.querySelectorAll('.container .calculator .keys-container .
 const input = document.querySelector('.container .calculator .screen .input')
 const output = document.querySelector('.container .calculator .screen .output')
 const equal = document.querySelector('.container .calculator .keys-container .equal-key')
+const c = document.querySelector('.container .calculator .keys-container .c-key')
+const ce = document.querySelector('.container .calculator .keys-container .ce-key')
+
 for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener('click', () =>  {
         input.innerHTML += keys[i].textContent
@@ -10,4 +13,10 @@ for (let i = 0; i < keys.length; i++) {
 }
 equal.addEventListener('click', () => {
     output.innerHTML = eval(input.innerHTML)
+})
+c.addEventListener('click', () => {
+    input.innerHTML = input.innerHTML.slice(0, -1)
+})
+ce.addEventListener('click', () => {
+    input.innerHTML = ''
 })
